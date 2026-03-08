c3a595313aa242bf16c389770c67e92f340d4a76
MYKNI <MYKNI@telenor.dk>
2026-03-08 19:54:29 +0200

    add sourceMap

---
+++ src/routes/typescript/tsconfig/sourceMap/demo.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+const d = sum(1, 3);
---
+++ src/routes/typescript/tsconfig/sourceMap/dist/demo.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var d = sum(1, 3);
+//# sourceMappingURL=demo.js.map
